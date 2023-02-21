d4f572c32fecec3834e345c84cd7ce744705ad8b
DG <gdavidcefim@gmail.com>
2023-02-21 09:13:08 +0100

    Correction step-1

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header>
+  <nav>
+    <ul class="menu">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="articles.php">Articles</a></li>
+      <li><a href="#">Connexion</a></li>
+    </ul>
+  </nav>
+</header>
